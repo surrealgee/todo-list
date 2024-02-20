2257c33e715067e268fedc8a52cbf61614dbe89c
Surreal Gee <94802496+surrealgee@users.noreply.github.com>
2024-02-19 20:41:28 -0400

    Modify view class

--- src/view.js
+++ src/view.js
@@ -1,32 +1,77 @@
 export default class View {
     constructor() {
+        this.root = this.getElement('#root');
+        this.header = this.createElement('header');
+        this.main = this.createElement('main');
+        this.logo = this.createElement('h1', { textContent: 'todo' });
+        this.form = this.createElement('form');
+        this.input = this.createElement('input', { type: 'text', name: 'title', placeholder: 'Create a new todo...' });
 
+        this.form.appendChild(this.input);
+        this.header.append(this.logo, this.form);
+        this.root.append(this.header, this.main);
+
+    };
+
+    // Creates a new element using the chosen "tag" and takes a "props" object to assign attributes to the element.
+    createElement(tag, props) {
+        const element = document.createElement(tag);
+
+        for (let prop in props) {
+            if (prop === 'class') {
+                element.classList.add(props[prop])
+            } else {
+                element[prop] = props[prop];
+            }
+        }
+        return element;
+    };
+
+    getElement(selector) {
+        const element = document.querySelector(selector);
+
+        return element;
     };
 
     render = (list) => {
-        const todosEl = document.querySelector('.todos');
+        const todoList = this.createElement('ul');
 
-        list.forEach(task => {
-            const listItem = document.createElement('li');
-            listItem.id = task.id;
+        todoList.innerHtml = '';
 
-            const checkboxElement = document.createElement('input');
-            checkboxElement.type = 'checkbox';
-            checkboxElement.checked = task.isDone;
-            checkboxElement.id = task.id;
+        if (list.length === 0) {
+            const para = this.createElement('li', { textContent: 'Nothing to do. Add a Task!' })
+            todoList.appendChild(para);
+        } else {
+            list.forEach(task => {
+                const listItem = this.createElement('li', { id: task.id });
 
-            const textElement = document.createElement('span');
-            textElement.textContent = task.title;
-            textElement.id = task.id;
+                const checkboxElement = this.createElement('input',
+                    {
+                        type: 'checkbox',
+                        checked: task.isDone,
+                    });
 
-            const deleteBtn = document.createElement('button');
-            deleteBtn.textContent = 'X'
+                const textElement = this.createElement('span',
+                    {
+                        textContent: task.title,
+                        class: task.isDone ? "done" : "undone",
+                    });
 
-            listItem.appendChild(checkboxElement);
-            listItem.appendChild(textElement);
-            listItem.appendChild(deleteBtn);
+                const deleteBtn = this.createElement('button', { textContent: 'X' });
 
-            todosEl.appendChild(listItem);
-        })
+                listItem.append(checkboxElement, textElement, deleteBtn);
+
+                todoList.appendChild(listItem);
+            })
+        }
+        this.main.appendChild(todoList);
     };
+
+    get _todoText() {
+        return this.input.value;
+    }
+
+    _resetInput() {
+        this.input.value = '';
+    }
 };
