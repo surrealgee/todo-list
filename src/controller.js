b691163a065fbfead6712f818bcfce132086ff11
Surreal Gee <94802496+surrealgee@users.noreply.github.com>
2024-02-25 15:32:44 -0400

    Build local storage support

--- src/controller.js
+++ src/controller.js
@@ -9,34 +9,28 @@ export default class Controller {
         this.view.bindRemoveTask(this.handleRemoveTask);
         this.view.bindToggleTask(this.handleToggleTask);
         this.view.bindEditTask(this.handleEditTask);
+
+        this.model.bindTodoListChange(this.onChange);
     };
 
-    onChange(taskList) {
+    onChange = (taskList) => {
         this.view.render(taskList);
         console.table(this.model.list);
     };
 
     handleAddTask = (taskName) => {
         this.model.addTask(taskName);
-
-        this.onChange(this.model.list);
     }
 
     handleEditTask = (id, newTitle) => {
         this.model.editTask(id, newTitle);
-
-        this.onChange(this.model.list);
     }
 
     handleRemoveTask = (id) => {
         this.model.removeTask(id);
-
-        this.onChange(this.model.list);
     }
 
     handleToggleTask = (id) => {
         this.model.toggleTask(id);
-
-        this.onChange(this.model.list);
     }
 };
--- src/model.js
+++ src/model.js
@@ -2,35 +2,47 @@ import { v4 as uuid } from 'uuid';
 
 export default class Model {
     constructor() {
-        this.list = [
-            new Task("Coding"),
-            new Task("Reading"),
-            new Task("Exercising"),
-            new Task("Flexing")
-        ];
+        this.list = JSON.parse(localStorage.getItem('todo_list')) || [];
     };
+
+    _commit(list) {
+        this.onTodoListChange(list);
+        localStorage.setItem('todo_list', JSON.stringify(list));
+    }
 
     addTask(taskName) {
         const newTask = new Task(taskName);
 
         this.list.push(newTask);
+
+        this._commit(this.list);
     };
 
     editTask(id, newTitle) {
         this.list = this.list.map(task => {
             return (task.id === id) ? { ...task, title: newTitle } : task;
         })
+
+        this._commit(this.list);
     };
 
     removeTask(id) {
         this.list = this.list.filter(task => task.id !== id);
+
+        this._commit(this.list);
     };
 
     toggleTask(id) {
         this.list = this.list.map(task => {
             return (task.id === id) ? { ...task, isDone: !task.isDone } : task;
         })
+
+        this._commit(this.list);
     };
+
+    bindTodoListChange(callback) {
+        this.onTodoListChange = callback;
+    }
 };
 
 class Task {
